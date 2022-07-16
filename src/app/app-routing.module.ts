bfc2d09434328e3406c7d4dc76cec64ae4358f4c
Suman Sen <mesuman@yahoo.com>
2022-07-17 00:45:12 +0600

    eid assignment complate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"contact",
+    component: ContactComponent
+  },
+  {
+    path:"about",
+    component: AboutComponent
+  },
+  {
+    path:"blog",
+    loadChildren: ()=>import ('./components/pages/blog/blog.module').then(m=>m.BlogModule),
+  },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: ErrorComponent },  // Wildcard route for a 404 page
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/inlcude/header/header.component';
 import { FooterComponent } from './components/inlcude/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { WorkComponent } from './components/pages/work/work.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ErrorComponent } from './components/pages/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    WorkComponent,
+    ContactComponent,
+    AboutComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,

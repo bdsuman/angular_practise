a0063331b3c8e963dea49e45ba4cd454a527aa14
Suman Sen <mesuman@yahoo.com>
2022-07-16 22:10:51 +0600

    header & footer added extrenal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/inlcude/header/header.component';
+import { FooterComponent } from './components/inlcude/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
